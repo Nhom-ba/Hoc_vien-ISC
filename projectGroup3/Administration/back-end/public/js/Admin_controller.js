34274f4f861113da0b5eb5cc50ecf644eae026fc
Thu Hien <ntthuhien1324@gmail.com>
2017-02-06 14:31:03 +0700

    Da load duoc du lieu cho trang Admin

--- projectGroup3/Administration/back-end/public/js/Admin_controller.js
+++ projectGroup3/Administration/back-end/public/js/Admin_controller.js
@@ -1,6 +1,15 @@
-app.controller("Banner_Control",function($scope){
-        $scope.msg = "This is banner controller";
+app.controller("Banner_Control",function($http,$scope){
+        $scope.banners=[];
+        $http.get("http://localhost:3000/banners")
+            .then(function(response){
+                $scope.banners = response.data;
+        });
+    
     });
-app.controller("News_Control",function($scope){
-		$scope.msg = "This is News controller";
+app.controller("News_Control",function($http,$scope){
+		$scope.news=[];
+        $http.get("http://localhost:3000/news")
+            .then(function(response){
+                $scope.news = response.data;
+        });
 	});
